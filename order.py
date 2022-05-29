64049beed5a57c2b2209be182222f3d4755857cc
Dean Hunter <jeeka7@gmail.com>
2022-05-30 00:10:38 +0530

    Update order.py

--- order.py
+++ order.py
@@ -12,3 +12,14 @@ with st.sidebar:
         "Choose a shipping method",
         ("Standard (5-15 days)", "Express (2-5 days)")
     )
+with st.form("my_form"):
+    st.write("Inside the form")
+    slider_val = st.slider("Form slider")
+    checkbox_val = st.checkbox("Form checkbox")
+
+    # Every form must have a submit button.
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        st.write("slider", slider_val, "checkbox", checkbox_val)
+
+st.write("Outside the form")
