35f235bc9712c8b3dc6c22d274e5213c5691edb3
Dean Hunter <jeeka7@gmail.com>
2022-05-30 17:57:30 +0530

    Update order.py

--- order.py
+++ order.py
@@ -5,7 +5,7 @@ import pandas as pd
 left_column, right_column = st.columns(2)
 # You can use a column just like st.sidebar:
 left_column.button('Press me!')
-
+    st.write("Bella Ciao Bella Ciao")
 # Or even better, call Streamlit functions inside a "with" block:
 with right_column:
     chosen = st.radio(
