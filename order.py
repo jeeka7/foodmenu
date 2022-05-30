2429c6287b93183cd3bb5cc10d53d2cd13d7e466
Dean Hunter <jeeka7@gmail.com>
2022-05-30 16:33:50 +0530

    Update order.py

--- order.py
+++ order.py
@@ -1,28 +1,5 @@
 import streamlit as st
 
-# Using object notation
-add_selectbox = st.sidebar.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone")
-)
-
-# Using "with" notation
-with st.sidebar:
-    add_radio = st.radio(
-        "Choose a shipping method",
-        ("Standard (5-15 days)", "Express (2-5 days)")
-    )
-with st.form("my_form"):
-    st.write("Inside the form")
-    slider_val = st.slider("Form slider")
-    checkbox_val = st.checkbox("Form checkbox")
-
-    # Every form must have a submit button.
-    submitted = st.form_submit_button("Submit")
-    if submitted:
-        st.write("slider", slider_val, "checkbox", checkbox_val)
-
-st.write("Outside the form")
 
 food = ['Green', 'Yellow', 'Red', 'Blue']
 options = st.multiselect(
